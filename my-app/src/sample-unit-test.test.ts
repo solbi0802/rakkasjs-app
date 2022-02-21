18a1da7a5b53f939670c5f60cdb197f5d237c82a
SolbiShin <sbshin.dev@gmail.com>
2022-02-22 00:09:24 +0900

    rakkasjs setup

---
+++ my-app/src/sample-unit-test.test.ts
@@ -0,0 +1,9 @@
+describe("Sample unit test", () => {
+  // @testing-library/react is also available for component testing
+  it("adds two and two correctly", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+// Must be a module
+export {};
